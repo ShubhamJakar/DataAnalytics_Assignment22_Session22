002fdb8f745f12fe3fcfba7f3b562d4feebdedca
Shubham Jakar <34440552+ShubhamJakar@users.noreply.github.com>
2018-03-28 03:46:45 +0530

    Add files via upload

---
+++ Assignment22_session22_Answer.R
@@ -0,0 +1,13 @@
+#Assignment22_Session22
+
+#Problem
+#1. Use the below given data set
+#   DataSet
+#2. Perform the below given activities:
+#a. apply K-means clustering to identify similar recipies
+#b. apply K-means clustering to identify similar attributes
+#c. how many unique recipies that people order often
+#d. what are their typical profiles
+
+
+#Answers
